33bd667e72575a6871329d05e9f74b995a386881
Tim Burns <tim.marshall.burns95@gmail.com>
2020-12-08 20:17:09 -0500

    some js fixed

--- scripts/roster.js
+++ scripts/roster.js
@@ -1,23 +1,28 @@
+//function to change color and font size of og
 function hover() {
     var x = document.getElementById("box a");
     x.style.color = 'gold'
     x.style.fontSize = 'xx-large'
 }
-
-
+//function to change back to og
 function out() {
     var x = document.getElementById("box a");
     x.style.color = 'white';
     x.style.fontSize = 'x-large'
 }
 
-
-function imageIN() {
-    var img = document.getElementById("profile");
-    img.style.maxWidth = '500px';
-}
-
-function imageOUT() {
-    var img = document.getElementById("profile");
-    img.style.maxWidth = '200px';
-}
+// Get the img object using its Id 
+img = document.getElementById("profile"); 
+// Function to increase image size 
+function enlargeImg() { 
+  // Set image size to 1.5 times original 
+  profile.style.transform = "scale(1.5)"; 
+  // Animation effect  
+  profile.style.transition = "transform 0.25s ease"; 
+} 
+// Function to reset image size 
+function reset() { 
+  // Set image size to original 
+  img.style.transform = "scale(1)"; 
+  img.style.transition = "transform 0.25s ease"; 
+} 
